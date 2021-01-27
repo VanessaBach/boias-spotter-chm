335bd27ad7134362c94b45e37bb85dc41b9bb5b5
vanessabach <vanessabach.r@gmail.com>
2021-01-27 10:27:50 -0300

    starting a plataform for all the spotters

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,65 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @values = api_spotter
+
+    # @markers = []
+    # @values[:latitude].each_with_index do |marker, idx|
+    #   @markers << { lat: @values[:latitude][idx], lng: @values[:longitude][idx], date_time: @values[:timestamp][idx] }
+    # end
+  end
+
+  private
+
+  def api_spotter
+
+    response = RestClient.get("http://remobsapi.herokuapp.com/api/v1/data_buoys?buoy=2&token=#{ENV["SPOTTER_TOKEN"]}")
+
+    spotter_response = JSON.parse(response)
+    latitude = []
+    longitude =[]
+    datetime =[]
+    windspeed = []
+    winddirection = []
+    significantwaveheight = []
+    peakperiod = []
+    meanperiod = []
+    wavedirection = []
+    peakdirection =[]     
+    watertemperature = []
+    battery = []
+    
+	  spotter_response[0].each do |item|
+      latitude << item["lat"]
+      longitude << item["lon"]
+      x = item["date_time"]
+      datetime = DateTime.parse x
+      datetime << datetime.strftime("%d-%m %H:%M")
+      windspeed << item["wspd"]
+      winddirection << item["wdir"]    
+      significantwaveheight << item["swvht1"]
+      peakperiod << item["tp1"]
+      meanperiod << item["mean_tp"]   
+      wavedirection << item["wvdir1"]
+      peakdirection << item["pk_dir"]
+      watertemperature << item ["sst"]   
+      battery << item ["battery"]      
+    end
+    
+    
+    params = {}
+    params[:latitude] = latitude
+    params[:longitude] = longitude
+    params[:datetime] = datetime
+    params[:windspeed] = windspeed
+    params[:winddirection] = winddirection
+    params[:significantwaveheight] = significantwaveheight
+    params[:peakperiod] = peakperiod
+    params[:meanperiod] = meanperiod
+    params[:wavedirection] = wavedirection
+    params[:peakdirection] = peakdirection
+    params[:watertemperature] = watertemperature    
+    params[:battery] = battery
+    params = params.to_h
   end
 end
